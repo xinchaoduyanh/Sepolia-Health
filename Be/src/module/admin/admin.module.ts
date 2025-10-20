b0386f87651e093c75e49c5cd0928ab51c1819a1
xinchaoduyanh <duyanh19122k3@gmail.com>
2025-10-20 10:34:27 +0700

    Update admin.module.ts

--- Be/src/module/admin/admin.module.ts
+++ Be/src/module/admin/admin.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AdminAuthModule } from './auth/admin-auth.module';
+import { AdminPatientModule } from './patient/admin-patient.module';
+import { AdminDoctorModule } from './doctor/admin-doctor.module';
+import { AdminReceptionistModule } from './receptionist/admin-receptionist.module';
+import { AdminStatisticsModule } from './statistics/admin-statistics.module';
 
 @Module({
-  imports: [AdminAuthModule],
-  exports: [AdminAuthModule],
+  imports: [
+    AdminAuthModule,
+    AdminPatientModule,
+    AdminDoctorModule,
+    AdminReceptionistModule,
+    AdminStatisticsModule,
+  ],
+  exports: [
+    AdminAuthModule,
+    AdminPatientModule,
+    AdminDoctorModule,
+    AdminReceptionistModule,
+    AdminStatisticsModule,
+  ],
 })
 export class AdminModule {}
