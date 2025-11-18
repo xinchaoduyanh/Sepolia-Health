24ac671456a048be5f5e8af3032d40d54c5a3a79
Phong <hungphongb@gmail.com>
2025-11-18 16:08:00 +0700

    fix(Be): forgot to add booking controller

--- Be/src/module/patient/appointment/appointment.module.ts
+++ Be/src/module/patient/appointment/appointment.module.ts
@@ -3,10 +3,11 @@ import { AppointmentController } from './appointment.controller';
 import { AppointmentService } from './appointment.service';
 import { PrismaModule } from '@/common/prisma/prisma.module';
 import { BullModule } from '@nestjs/bullmq';
+import { BookingController } from './booking.controller';
 
 @Module({
   imports: [PrismaModule, BullModule.registerQueue({ name: 'appointment' })],
-  controllers: [AppointmentController],
+  controllers: [AppointmentController, BookingController],
   providers: [AppointmentService],
   exports: [AppointmentService],
 })
