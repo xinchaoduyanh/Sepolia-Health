e06e5bfb7584ea3dfd07d1b80402398de2fd3a60
foq <hungphongb@gmail.com>
2025-12-04 00:14:04 +0700

    chore(app+web): update image

--- web/packages/assets/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as DoctorPana } from './Doctor-pana.png'
-export { default as SepoliaIcon } from './sepolia-icon.png'
-
-export const SepoliaImages = {
-    DoctorPana: require('./Doctor-pana.png'),
-    SepoliaIcon: require('./sepolia-icon.png'),
-    PepeOpenEye: require('./pepe_open_eye.png'),
-    PepeCoverEye: require('./pepe_cover_eye.png'),
-}
